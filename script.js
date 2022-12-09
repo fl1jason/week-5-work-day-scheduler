4069550ab75cec79ade4c52dd7ac7b212f75dd21
fl1jason <jason@fl1digital.com>
2022-12-09 13:51:05 +0000

    fixed logic and added Save

--- script.js
+++ script.js
@@ -36,43 +36,22 @@ function onSaveTask(e) {
 
 function generateTimeslots() {
 
-    for (var hour = options.startHour; hour <= options.endHour; hour++) {
+    for (hour = options.startHour; hour <= options.endHour; hour++) {
 
         // load the task from local storage
-        var savedTask = localStorage.getItem(hour);
-
-        var timeSlot = $('<div>').addClass('row time-block');
-        timeSlot.attr('data-hour', hour);
-
-        var hourSlot = $('<div>').addClass('col-sm-2 hour').text(moment(hour, 'h').format('h A'));
-        var description = $('<div>').addClass('col-sm-8 row');
-        var textArea = $('<textarea>').addClass('col-md-12 description');
-        textArea.val(savedTask);
-
-        var saveContainer = $('<div>').addClass('saveBtn d-flex justify-content-center align-items-center col-1');
-        saveContainer.on('click', onSaveTask);
-        var saveButton = $('<i>').addClass('fas fa-save');
-
-
-        timeSlot.append(hourSlot);
-        timeSlot.append(description);
-        description.append(textArea);
-        timeSlot.append(saveContainer);
-        saveContainer.append(saveButton);
-
-        $('.container').append(timeSlot);
+        var savedTask = localStorage.getItem(hour) || '';
+        var html = `<div class="row" data-hour="${hour}">
+            <div class="col-sm-2 hour">${hour}</div>
+            <div class="col-sm-8 row past">
+                <textarea class="col-md-10 description">${savedTask}</textarea>
+            </div>
+            <div class="col-sm-2">
+                <button class="btn btn-primary saveBtn">Save</button>
+            </div>
+        </div>
+    `
+        $('.container').append(html);
     }
-    /*
-    <div class="row">
-        <div class="col-sm-2 hour">9AM</div>
-        <div class="col-sm-8 row past">
-            <textarea class="col-md-10 description"></textarea>
-        </div>
-        <div class="col-sm-2">
-            <button class="btn btn-primary saveBtn">Save</button>
-        </div>
-    </div>
-    */
 }
 
 function init() {
@@ -82,6 +61,9 @@ function init() {
 
     // Update the timeslot's backgroud colours based ont he time of day
     updateTimeslots();
+
+    // Set up the save button
+    $('.saveBtn').on('click', onSaveTask);
 
     // Set the current day
     var currentDay = moment().format('dddd MMMM Do YYYY, h:mm:ss a');
